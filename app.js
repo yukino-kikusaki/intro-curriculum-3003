427c411c7a43ce4bedd6a0c192f770597cb9c2f1
Azumabashi <hiro_kataoka@hkataoka.local>
2023-04-15 16:50:59 +0900

    update program

--- app.js
+++ app.js
@@ -4,38 +4,40 @@ const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-rl.on('line', (lineString) => {
+rl.on('line', lineString => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
   const prefecture = columns[1];
   const popu = parseInt(columns[3]);
-  if (year === 2010 || year === 2015) {
-    let value = prefectureDataMap.get(prefecture);
-    if (!value) {
+  if (year === 2016 || year === 2021) {
+    let value = null;
+    if (prefectureDataMap.has(prefecture)) {
+      value = prefectureDataMap.get(prefecture);
+    } else {
       value = {
-        popu10: 0,
-        popu15: 0,
+        before: 0,
+        after: 0,
         change: null
       };
     }
-    if (year === 2010) {
-      value.popu10 = popu;
+    if (year === 2016) {
+      value.before = popu;
     }
-    if (year === 2015) {
-      value.popu15 = popu;
+    if (year === 2021) {
+      value.after = popu;
     }
     prefectureDataMap.set(prefecture, value);
   }
 });
 rl.on('close', () => {
   for (const [key, value] of prefectureDataMap) {
-    value.change = value.popu15 / value.popu10;
+    value.change = value.after / value.before;
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
     return pair2[1].change - pair1[1].change;
   });
   const rankingStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.popu10}=>${value.popu15} 変化率: ${value.change}`;
+    return `${key}: ${value.before}=>${value.after} 変化率: ${value.change}`;
   });
   console.log(rankingStrings);
 });
